4ee8927dc0c383cd65faaa0ff1d43452064fe668
Palkitch <125451425+Palkitch@users.noreply.github.com>
2023-10-15 12:10:38 +0200

    test propojení s databází

--- Nemocnice/MainWindow.xaml.cs
+++ Nemocnice/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Nemocnice
 {
@@ -23,6 +25,31 @@ namespace Nemocnice
         public MainWindow()
         {
             InitializeComponent();
+            OracleConnection connection = GetConnection();
+            connection.Open();
+            PrintNameColumnValues(connection, "pacienti");
+        }
+        public static OracleConnection GetConnection()
+        {
+            string connectionString = "User Id=st67082;" +
+                "Password=abcde;" +
+                "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=fei-sql3.upceucebny.cz)(PORT=1521))(CONNECT_DATA=(SID=BDAS)(SERVER=DEDICATED)))";
+            return new OracleConnection(connectionString);
+        }
+        public void PrintNameColumnValues(OracleConnection connection, string tableName)
+        {
+            using (var command = new OracleCommand($"SELECT jmeno FROM {tableName} WHERE id_doktor = 10 ORDER BY id_pacient ", connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader.GetString(0);
+                        //Console.WriteLine("Name: " + name);
+                        label123.Content += name + "\n";
+                    }
+                }
+            }
         }
     }
 }
