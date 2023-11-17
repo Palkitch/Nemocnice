47257f084231371e0fe3793348cfeed45d9d04ba
Jakub Kodytek <jakub.kodytek@email.cz>
2023-11-17 15:13:35 +0100

    Add vkladani a načitani binárního contentu z DB (obrazky)

--- Nemocnice/Database/DatabaseHandler.cs
+++ Nemocnice/Database/DatabaseHandler.cs
@@ -5,10 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Nemocnice.Database
 {
@@ -26,13 +29,13 @@ namespace Nemocnice.Database
             Connection = DatabaseConnection.OracleConnection;
             Connection.Open();
         }
+
         public void ComboBoxHandle(ref ComboBox comboBox)
         {
-
             {
                 using (OracleCommand command = new OracleCommand("ZobrazeniTabulek", Connection))
                 {
-                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandType = CommandType.StoredProcedure;
 
                     command.Parameters.Add("result_cursor", OracleDbType.RefCursor, ParameterDirection.Output);
 
@@ -42,8 +45,8 @@ namespace Nemocnice.Database
                     StringBuilder sb = new StringBuilder();
                     while (reader.Read())
                     {
-                        string aliasTableName = reader["alias_table_name"].ToString();
-                        string tableName = reader["table_name"].ToString();
+                        string? aliasTableName = reader["alias_table_name"].ToString();
+                        string? tableName = reader["table_name"].ToString();
                         sb.AppendLine(tableName + " (" + aliasTableName + ")");
                         TableAliasMapping.Add(tableName, aliasTableName);
                     }
@@ -65,11 +68,11 @@ namespace Nemocnice.Database
             using (var command = new OracleCommand("ZiskaniDat", Connection))
             {
                 command.CommandType = CommandType.StoredProcedure;
-                string selectedTableAlias = comboBox.SelectedItem.ToString();
-                string dictValue = TableAliasMapping.FirstOrDefault(x => x.Value == selectedTableAlias).Key;
+                string? selectedTableAlias = comboBox.SelectedItem.ToString();
+                string? dictValue = TableAliasMapping.FirstOrDefault(x => x.Value == selectedTableAlias).Key;
 
                 // Převedení názvu tabulky na velká písmena
-                string tableName = dictValue.ToUpper();
+                string? tableName = dictValue.ToUpper();
 
                 command.CommandType = CommandType.StoredProcedure;
 
@@ -93,10 +96,10 @@ namespace Nemocnice.Database
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
                                     int postNum = int.Parse(ReadString(reader, 1));
-                                    string street = ReadString(reader, 2);
-                                    string city = ReadString(reader, 3);
+                                    string? street = ReadString(reader, 2);
+                                    string? city = ReadString(reader, 3);
                                     int postCode = int.Parse(ReadString(reader, 4));
-                                    string country = ReadString(reader, 5);
+                                    string? country = ReadString(reader, 5);
                                     Adresa address = new Adresa(id, postNum, street, city, country, postCode);
                                     collection.Add(address);
                                 }
@@ -106,7 +109,7 @@ namespace Nemocnice.Database
                             case "BUDOVY":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
+                                    string? name = ReadString(reader, 1);
                                     int? floors = ParseNullableInt(ReadString(reader, 2));
                                     int addressId = int.Parse(ReadString(reader, 3));
                                     Budova building = new Budova(id, name, floors, addressId);
@@ -119,7 +122,7 @@ namespace Nemocnice.Database
                             case "DIAGNOZY_CISELNIK":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
+                                    string? name = ReadString(reader, 1);
                                     Diagnoza diagnosis = new Diagnoza(id, name);
                                     collection.Add(diagnosis);
                                     break;
@@ -128,7 +131,7 @@ namespace Nemocnice.Database
 
                             case "DOKTORI":
                                 {
-                                    string sql = "SELECT z.* FROM ZAMESTNANCI z JOIN DOKTORI d ON z.id_zamestnanec = d.id_zamestnanec";
+                                    string? sql = "SELECT z.* FROM ZAMESTNANCI z JOIN DOKTORI d ON z.id_zamestnanec = d.id_zamestnanec";
                                     using (var cmd = new OracleCommand(sql, Connection))
                                     {
                                         using (var cmdReader = cmd.ExecuteReader())
@@ -136,8 +139,8 @@ namespace Nemocnice.Database
                                             while (cmdReader.Read())
                                             {
                                                 int id = int.Parse(ReadString(cmdReader, 0));
-                                                string name = ReadString(cmdReader, 1);
-                                                string surName = ReadString(cmdReader, 2);
+                                                string? name = ReadString(cmdReader, 1);
+                                                string? surName = ReadString(cmdReader, 2);
                                                 int salary = int.Parse(ReadString(cmdReader, 3));
                                                 int hospWardId = int.Parse(ReadString(cmdReader, 4));
                                                 int? superiorId = ParseNullableInt(ReadString(cmdReader, 5));
@@ -157,8 +160,8 @@ namespace Nemocnice.Database
                             case "LEKY":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
-                                    string category = ReadString(reader, 2);
+                                    string? name = ReadString(reader, 1);
+                                    string? category = ReadString(reader, 2);
                                     int price = int.Parse(ReadString(reader, 3));
                                     Lek medicament = new Lek(id, name, category, price);
                                     collection.Add(medicament);
@@ -181,7 +184,7 @@ namespace Nemocnice.Database
                             case "ODDELENI":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
+                                    string? name = ReadString(reader, 1);
                                     int buildingId = int.Parse(ReadString(reader, 2));
                                     Oddeleni oddeleni = new Oddeleni(id, name, buildingId);
                                     collection.Add(oddeleni);
@@ -191,13 +194,13 @@ namespace Nemocnice.Database
                             case "PACIENTI":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
-                                    string surName = ReadString(reader, 2);
+                                    string? name = ReadString(reader, 1);
+                                    string? surName = ReadString(reader, 2);
                                     DateTime birthDate = DateTime.Parse(ReadString(reader, 3));
-                                    string formattedBirthDate = birthDate.ToString("yyyy-MM-dd");
-                                    string pin = ReadString(reader, 4);
+                                    string? formattedBirthDate = birthDate.ToString("yyyy-MM-dd");
+                                    string? pin = ReadString(reader, 4);
                                     DateTime startDate = DateTime.Parse(ReadString(reader, 5));
-                                    string formattedStartDate = startDate.ToString("yyyy-MM-dd");
+                                    string? formattedStartDate = startDate.ToString("yyyy-MM-dd");
                                     int doctorId = int.Parse(ReadString(reader, 6));
                                     int addressId = int.Parse(ReadString(reader, 7));
                                     int insuranceId = int.Parse(ReadString(reader, 8));
@@ -209,7 +212,7 @@ namespace Nemocnice.Database
                             case "POJISTOVNY":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
+                                    string? name = ReadString(reader, 1);
                                     int code = int.Parse(ReadString(reader, 2));
                                     Pojistovna pojistovna = new Pojistovna(id, name, code);
                                     collection.Add(pojistovna);
@@ -229,7 +232,7 @@ namespace Nemocnice.Database
                             case "POMUCKY":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
+                                    string? name = ReadString(reader, 1);
                                     int count = int.Parse(ReadString(reader, 2));
                                     Pomucka pomucka = new Pomucka(id, name, count);
                                     collection.Add(pomucka);
@@ -242,7 +245,7 @@ namespace Nemocnice.Database
                                     int docId = int.Parse(ReadString(reader, 1));
                                     int patId = int.Parse(ReadString(reader, 2));
                                     DateTime date = DateTime.Parse(ReadString(reader, 3));
-                                    string formattedDate = date.ToString("yyyy-MM-dd");
+                                    string? formattedDate = date.ToString("yyyy-MM-dd");
                                     Recept prescription = new Recept(id, docId, patId, formattedDate);
                                     collection.Add(prescription);
                                     break;
@@ -255,8 +258,8 @@ namespace Nemocnice.Database
                             case "ZAMESTNANCI":
                                 {
                                     int id = int.Parse(ReadString(reader, 0));
-                                    string name = ReadString(reader, 1);
-                                    string surname = ReadString(reader, 2);
+                                    string? name = ReadString(reader, 1);
+                                    string? surname = ReadString(reader, 2);
                                     int salary = int.Parse(ReadString(reader, 3));
                                     int wardId = int.Parse(ReadString(reader, 4));
                                     int? superiorId = ParseNullableInt(ReadString(reader, 5));
@@ -271,6 +274,76 @@ namespace Nemocnice.Database
                 }
             }
             grid.ItemsSource = collection;
+        }
+
+        public void saveImageToDatabase(string filePath)
+        {
+            try
+            {
+                byte[] imageBytes = LoadImageBytes(filePath);
+                string? imageName = getImageNameFromFilePath(filePath);
+
+                using (OracleCommand cmd = new OracleCommand())
+                {
+                    cmd.Connection = Connection;
+                    cmd.CommandText = "INSERT INTO Ikony (nazev, obsah) VALUES (:imageName, :image)";
+                    cmd.Parameters.Add("imageName", OracleDbType.Varchar2).Value = imageName;
+                    cmd.Parameters.Add("image", OracleDbType.Blob).Value = imageBytes;
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Obrázek byl úspěšně uložen do databáze.", "Info");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba při ukládání obrázku: " + ex.Message, "Chyba");
+            }
+        }
+
+        public BitmapImage loadImageFromDatabase() 
+        {
+            try
+            {
+                using (OracleCommand cmd = new OracleCommand())
+                {
+                    cmd.Connection = Connection;
+                    cmd.CommandText = "SELECT obsah FROM Ikony WHERE id_obrazek = 3";
+                    OracleDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        // Načítání bytů z databáze
+                        byte[] imageBytes = (byte[])reader["obsah"];
+
+                        // Vytvoření BitmapImage z načtených bytů
+                        BitmapImage bitmap = new BitmapImage();
+                        bitmap.BeginInit();
+                        bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmap.StreamSource = new MemoryStream(imageBytes);
+                        bitmap.EndInit();
+                        return bitmap;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba při načítání obrázku: " + ex.Message, "Chyba");
+            }
+            return null;
+        }
+
+        private string? getImageNameFromFilePath(string filePath)
+        {
+            string[] parts = filePath.Split('\\', '/');
+            string fileName = parts[parts.Length - 1];
+            return fileName;
+        }
+
+        private byte[] LoadImageBytes(string filePath)
+        {
+            byte[] imageBytes = File.ReadAllBytes(filePath);
+            return imageBytes;
         }
 
 
--- Nemocnice/MainWindow.xaml.cs
+++ Nemocnice/MainWindow.xaml.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
-using System.Windows;
 using Nemocnice.ModelObjects;
 using Oracle.ManagedDataAccess.Client;
 using System.Windows.Controls;
 using System.Windows.Data;
 using Nemocnice.Database;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace Nemocnice
 {
@@ -36,7 +39,7 @@ namespace Nemocnice
             }
             else  // Uživatel zavřel dialogové okno - ukončí aplikaci
             {
-                Application.Current.Shutdown();
+                System.Windows.Application.Current.Shutdown();
             }
 
             // mainwindow center possition
@@ -50,6 +53,31 @@ namespace Nemocnice
         private void printButtonOnAction(object sender, RoutedEventArgs e)
         {
             handler.SwitchMethod(ref comboBox, ref grid);
+        }
+
+        private void profileInsertPictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Obrázky|*.jpg;*.jpeg;*.png|Všechny soubory|*.*";
+
+                // Nastavíme počáteční adresář pro prohlížeč souborů (volitelné)
+                openFileDialog.InitialDirectory = "C:\\";
+
+                // Zobrazíme dialog pro výběr souboru
+                DialogResult result = openFileDialog.ShowDialog();
+
+                // Pokud uživatel vybere soubor a potvrdí dialog, zpracujeme vybraný soubor
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    string selectedFilePath = openFileDialog.FileName;
+                    System.Windows.MessageBox.Show("Načteno: " + selectedFilePath);
+
+                    handler.saveImageToDatabase(selectedFilePath);
+
+                    profileImg.Source = handler.loadImageFromDatabase();
+                }
+            }
         }
     }
 }
